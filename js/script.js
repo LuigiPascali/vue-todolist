5a5797268c722e9509fb472a7f8e8abd3217e7a3
luigimariapascali <luigimariapascali@gmail.com>
2023-07-04 18:36:49 +0200

    bonus 2 complete, exercize done!

--- js/script.js
+++ js/script.js
@@ -3,11 +3,17 @@ const app = Vue.createApp({
   data() {
     return {
       // Dal momento che ho inserito la possibilità di scrivere le cose da fare, ho svuotato l'array. 
-      todos: [],
+      // Per fare il BONUS 2, inserisco nuovamente nell'array delle cose da fare:
+      todos: [
+        { text: 'Studiare bene Bootstrap', done: true },
+        { text: 'Studiare bene Vue JS', done: false },
+        { text: 'Finire questo esercizio', done: true },
+        { text: 'Iniziare a studiare Python', done: true },
+        { text: 'Diventare sempre più autonomo nella scrittura dei code', done:false },
+      ],
       newTodoText: ''
     }
   },
-  
   methods: {
     // MILESTONE 3:
     addTodo() {
@@ -18,7 +24,6 @@ const app = Vue.createApp({
         console.log("Lista aggiornata:", this.todos);
       }
     },
-
     // MILESTONE 2:
     removeTodo(todo) {
       const index = this.todos.indexOf(todo);
@@ -27,7 +32,13 @@ const app = Vue.createApp({
       }
       console.log("To-Do rimosso:", todo);
       console.log("Lista aggiornata:", this.todos);
-    }
+    },
+    // BONUS 2:
+    toggleTodoDone(todo) {
+      todo.done = !todo.done;
+      console.log("To-Do modificato:", todo);
+      console.log("Lista aggiornata:", this.todos);
+    },
   },
   mounted() {
     console.log("Lista iniziale:", this.todos);
